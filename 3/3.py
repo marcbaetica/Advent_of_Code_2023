d6a3f0d3bfb95aaddff32120886201482f09d4e2
marcbaetica <marc.baetica@gmail.com>
2024-09-14 16:55:53 -0400

    Day 3 WIP 2.

--- 3/3.py
+++ 3/3.py
@@ -1,21 +1,27 @@
+import re
 from lib.lib import read_input_lines
 from pprintpp import pprint
 
 
-board = read_input_lines('in_a.txt')
-pprint(board)
+# schematic = read_input_lines('in_a.txt')
+# pprint(schematic)
+
+with open('in_a.txt', 'r') as f:
+    schematic = [line.rstrip() for line in f.readlines()]
+pprint(schematic)
 
 # find coordinates of symbols
-symbols = {}
-for row, line in enumerate(board):
-    for column, char in enumerate(line):
-        if not char.isnumeric() and not char == '.':
-            symbols[row, column] = char
-
+symbols = {(r, c): [] for r in range(len(schematic)) for c in range(len(schematic[r])) if schematic[r][c] not in '0123456789.'}
 pprint(symbols)
 
-# find numbers
+# find all numbers
+# import re
+#
+# matches = {match for line in schematic for match in re.finditer('\d+', line)}
+# pprint(matches)
+
 
 # find numbers associated with symbols without duplicates
-
+symbol_boundaries = {(r, c): {(r-1, r, r+1), (c-1, c, c+1)} for r, c in symbols.keys()}
+pprint(symbol_boundaries)
 # add the numbers
