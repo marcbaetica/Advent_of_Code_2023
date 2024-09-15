151edacff90726b11481ef376e8665ad6d68007b
marcbaetica <marc.baetica@gmail.com>
2024-09-15 16:38:51 -0400

    Day 3 WIP 2.

--- 3/3.py
+++ 3/3.py
@@ -3,21 +3,16 @@ from lib.lib import read_input_lines
 from pprintpp import pprint
 
 
-# schematic = read_input_lines('in_a.txt')
-# pprint(schematic)
-
-with open('in_a.txt', 'r') as f:
-    schematic = [line.rstrip() for line in f.readlines()]
+schematic = read_input_lines('in_a.txt')
 pprint(schematic)
+
 
 # find coordinates of symbols
 symbols = {(r, c): [] for r in range(len(schematic)) for c in range(len(schematic[r])) if schematic[r][c] not in '0123456789.'}
 pprint(symbols)
 
 # find all numbers
-# import re
-#
-# matches = {match for line in schematic for match in re.finditer('\d+', line)}
+
 # pprint(matches)
 
 
